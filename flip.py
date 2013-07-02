4409be7b39943a5fd0c8211026c8e9a28ab41655
Lucas Cherkewski <lucas@ecustom.ca>
2013-07-02 19:55:54 -0400

    Use spaces instead of tabs. (as suggested by python linter)

--- flip.py
+++ flip.py
@@ -20,68 +20,68 @@ args = parser.parse_args()
 print("The following is a program that simulates a coin flip a certain amount of times, and displays the results.")
 
 def askHowManyTimesToRepeat():
-	timesToRepeat = int(input("Enter the amount of times you would like the coin to be flipped: "))
-	return timesToRepeat
+    timesToRepeat = int(input("Enter the amount of times you would like the coin to be flipped: "))
+    return timesToRepeat
 
 def askToLogOrNot():
-	logFileYesOrNo = input("Would you like to save a logfile? Y/N: ")
-	if logFileYesOrNo.lower() == "y":
-		logFileName = input("Log file filename: ")
-	else:
-		logFileName = "n"
-	return logFileName
+    logFileYesOrNo = input("Would you like to save a logfile? Y/N: ")
+    if logFileYesOrNo.lower() == "y":
+        logFileName = input("Log file filename: ")
+    else:
+        logFileName = "n"
+    return logFileName
 
 def askToShowVisualOutput():
-	visualOutputYesOrNo = input("Would you like to visually output the result of each toss? Y/N: ").lower()
-	return visualOutputYesOrNo
+    visualOutputYesOrNo = input("Would you like to visually output the result of each toss? Y/N: ").lower()
+    return visualOutputYesOrNo
 
 if args.n == None:
-	timesToRepeat = askHowManyTimesToRepeat()
+    timesToRepeat = askHowManyTimesToRepeat()
 else:
-	timesToRepeat = int(args.n[0])
+    timesToRepeat = int(args.n[0])
 
 if args.l == None:
-	logFileName = askToLogOrNot()
+    logFileName = askToLogOrNot()
 elif args.l[0].lower() != "no" or args.l[0].lower() != "n":
-	logFileName = args.l[0]
+    logFileName = args.l[0]
 else:
-	logFileName = "n"
+    logFileName = "n"
 
 if args.o == None:
-	visualOutputYesOrNo = askToShowVisualOutput()
+    visualOutputYesOrNo = askToShowVisualOutput()
 else:
-	visualOutputYesOrNo = args.o[0].lower()
+    visualOutputYesOrNo = args.o[0].lower()
 
 while i < timesToRepeat:
-	flip = random.randrange(2)
-	flipped = flipped+1
-	i=i+1
-	if flip == 0:
-		heads = heads+1
-		tempHeadValue = tempHeadValue+1
-		if longTailValue < tempTailValue:
-			longTailValue = tempTailValue
-			tempTailValue = 0
-		else:
-			tempTailValue = 0
-	else:
-		tails = tails+1
-		tempTailValue = tempTailValue+1
-		if longHeadValue < tempHeadValue:
-			longHeadValue = tempHeadValue
-			tempHeadValue = 0
-		else:
-			tempHeadValue = 0
-	if visualOutputYesOrNo == "y":
-		print(flip, end=" ")
+    flip = random.randrange(2)
+    flipped = flipped+1
+    i=i+1
+    if flip == 0:
+        heads = heads+1
+        tempHeadValue = tempHeadValue+1
+        if longTailValue < tempTailValue:
+            longTailValue = tempTailValue
+            tempTailValue = 0
+        else:
+            tempTailValue = 0
+    else:
+        tails = tails+1
+        tempTailValue = tempTailValue+1
+        if longHeadValue < tempHeadValue:
+            longHeadValue = tempHeadValue
+            tempHeadValue = 0
+        else:
+            tempHeadValue = 0
+    if visualOutputYesOrNo == "y":
+        print(flip, end=" ")
 tailspercent = (100/timesToRepeat)*tails
 headspercent = (100/timesToRepeat)*heads
 if tails > heads:
-	difference = tails - heads
+    difference = tails - heads
 elif heads > tails:
-	difference = heads - tails
+    difference = heads - tails
 else:
-	difference = 0
+    difference = 0
 
 elapsedtime = str(datetime.timedelta(seconds=round(time.time()-starttime,2)))
 elapsedtime = elapsedtime[:-4]
@@ -99,16 +99,16 @@ print("The longest string of heads in a row was:", longHeadValue)
 print("The amount of time taken to flip each coin on average was", estimate)
 
 if logFileName.lower() != "n":
-	logfile = open(os.getcwd()+'/'+logFileName, 'w')
-	logfile.write("Flipped a coin " + str(timesToRepeat) + " times.\n")
-	logfile.write("Total execution time: " + str(elapsedtime) + "\n")
-	logfile.write("Tails was flipped " + str(tails) + " times, or " + str(tailspercent) + "% of the time.\n")
-	logfile.write("Heads was flipped " + str(heads) + " times, or " + str(headspercent) + "% of the time.\n")
-	logfile.write("The difference between the two was " + str(difference) + ".\n")
-	logfile.write("The longest string of tails in a row was " + str(longTailValue) + ".\n")
-	logfile.write("The longest string of heads in a row was " + str(longHeadValue) + ".\n")
-	logfile.write("The amount of time taken to flip each coin on average was " + str(estimate) +".\n")
-	logfile.close()
-	print("A logfile was published to ", os.getcwd()+'/'+logFileName)
+    logfile = open(os.getcwd()+'/'+logFileName, 'w')
+    logfile.write("Flipped a coin " + str(timesToRepeat) + " times.\n")
+    logfile.write("Total execution time: " + str(elapsedtime) + "\n")
+    logfile.write("Tails was flipped " + str(tails) + " times, or " + str(tailspercent) + "% of the time.\n")
+    logfile.write("Heads was flipped " + str(heads) + " times, or " + str(headspercent) + "% of the time.\n")
+    logfile.write("The difference between the two was " + str(difference) + ".\n")
+    logfile.write("The longest string of tails in a row was " + str(longTailValue) + ".\n")
+    logfile.write("The longest string of heads in a row was " + str(longHeadValue) + ".\n")
+    logfile.write("The amount of time taken to flip each coin on average was " + str(estimate) +".\n")
+    logfile.close()
+    print("A logfile was published to ", os.getcwd()+'/'+logFileName)
 else:
-	print("No log created.")
+    print("No log created.")
