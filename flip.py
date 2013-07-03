90de077e5d32a384090c9fba88db3bd65111dfcb
Lucas Cherkewski <lucas@ecustom.ca>
2013-07-02 20:06:36 -0400

    Whitespace around operators, file newline.

--- flip.py
+++ flip.py
@@ -55,7 +55,7 @@ else:
 while i < timesToRepeat:
     flip = random.randrange(2)
     flipped = flipped+1
-    i=i+1
+    i = i+1
     if flip == 0:
         heads = heads+1
         tempHeadValue = tempHeadValue+1
@@ -83,9 +83,9 @@ elif heads > tails:
 else:
     difference = 0
 
-elapsedtime = str(datetime.timedelta(seconds=round(time.time()-starttime,2)))
+elapsedtime = str(datetime.timedelta(seconds=round(time.time()-starttime, 2)))
 elapsedtime = elapsedtime[:-4]
-seconds = round(time.time()-starttime,2)
+seconds = round(time.time()-starttime, 2)
 estimate = seconds/timesToRepeat
 
 print("\n\n")
@@ -107,7 +107,7 @@ if logFileName.lower() != "n":
     logfile.write("The difference between the two was " + str(difference) + ".\n")
     logfile.write("The longest string of tails in a row was " + str(longTailValue) + ".\n")
     logfile.write("The longest string of heads in a row was " + str(longHeadValue) + ".\n")
-    logfile.write("The amount of time taken to flip each coin on average was " + str(estimate) +".\n")
+    logfile.write("The amount of time taken to flip each coin on average was " + str(estimate) + ".\n")
     logfile.close()
     print("A logfile was published to ", os.getcwd()+'/'+logFileName)
 else:
